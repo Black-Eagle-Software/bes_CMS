c91d1d652ef7ab2e3bcb17bfc4d58d6c600e91b9
gary ramsey <gsramsey@gmail.com>
2018-11-18 15:37:33 -0500

    redid limit requests to use special sql queries always returns the last # of items

--- routes/tags/all.js
+++ routes/tags/all.js
@@ -1,14 +1,16 @@
 module.exports = (req, res)=>{
-    res.locals.connection.query("SELECT * FROM tags", (error, results, fields)=>{
-        if(error) throw error;
-        //res.status(200).send(JSON.stringify(results));
-        let limit = req.query.limit * 1;
-        let resultsCount = results.length;
-        let outputs = results;
-        if(resultsCount > limit){
-            outputs = results.slice(resultsCount - limit, resultsCount);
-        }        
-        res.status(200).send(JSON.stringify(outputs));
-        res.locals.connection.end();
-    });
+    let limit = req.query.limit * 1;
+    if(limit){
+        res.locals.connection.query("SELECT * FROM tags ORDER BY id DESC LIMIT ?", [limit], (error, results, fields)=>{
+            if(error) throw error;
+            res.status(200).send(JSON.stringify(results));
+            res.locals.connection.end();
+        });
+    }else{
+        res.locals.connection.query("SELECT * FROM tags", (error, results, fields)=>{
+            if(error) throw error;
+            res.status(200).send(JSON.stringify(results));            
+            res.locals.connection.end();
+        });
+    }
 };
