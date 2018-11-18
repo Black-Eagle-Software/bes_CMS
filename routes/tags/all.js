a9a70258bc5f32add14a0ba3aaac3829546c2f05
gary ramsey <gsramsey@gmail.com>
2018-11-18 15:10:40 -0500

    add limits to 'all' tag routing

--- routes/tags/all.js
+++ routes/tags/all.js
@@ -1,7 +1,14 @@
 module.exports = (req, res)=>{
     res.locals.connection.query("SELECT * FROM tags", (error, results, fields)=>{
         if(error) throw error;
-        res.status(200).send(JSON.stringify(results));
+        //res.status(200).send(JSON.stringify(results));
+        let limit = req.query.limit * 1;
+        let resultsCount = results.length;
+        let outputs = results;
+        if(resultsCount > limit){
+            outputs = results.slice(resultsCount - limit, resultsCount);
+        }        
+        res.status(200).send(JSON.stringify(outputs));
         res.locals.connection.end();
     });
 };
--- routes/tags/index.js
+++ routes/tags/index.js
@@ -2,7 +2,7 @@ const tags = require('express').Router();
 const all = require('./all');
 const single = require('./single');
 
-tags.get('/', all);
+tags.get('/', all); //handles limits as well
 tags.get('/:id', single);
 
 module.exports = tags;
