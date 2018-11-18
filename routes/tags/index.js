ea40e906e2ead306cdeb66d17de5248ab01c6672
gary ramsey <gsramsey@gmail.com>
2018-11-18 16:01:29 -0500

    add range of results for tags

--- routes/tags/index.js
+++ routes/tags/index.js
@@ -1,8 +1,10 @@
 const tags = require('express').Router();
 const all = require('./all');
+const range = require('./range');
 const single = require('./single');
 
 tags.get('/', all); //handles limits as well
+tags.get('/:from-:to', range);
 tags.get('/:id', single);
 
 module.exports = tags;
---
+++ routes/tags/range.js
@@ -0,0 +1,10 @@
+module.exports = (req, res)=>{
+    let from = req.params.from * 1;
+    let to = req.params.to * 1;
+    let distance = to - from;
+    res.locals.connection.query("SELECT * FROM tags ORDER BY id ASC LIMIT ?, ?", [from, distance + 1], (error, results, fields)=>{
+        if(error) throw error;
+        res.status(200).send(JSON.stringify(results));
+        res.locals.connection.end();
+    });
+};
