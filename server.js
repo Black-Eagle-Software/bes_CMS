201271e036a6ff8529069e7330f6fbb7032b6e3a
gary ramsey <gsramsey@gmail.com>
2018-11-18 21:37:10 -0500

    added server logging added uuid to each server request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const mysql = require('mysql');
+const uuid = require('uuid/v4');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,6 +17,20 @@ app.use((req, res, next)=>{
         database: "besCMS"
     });
     res.locals.connection.connect();
+    next();
+});
+
+app.use((req, res, next)=>{
+    req.uuid = uuid();
+    let host = req.get('x-forwarded-host') || '';
+    let forwarded = req.get('x-forwarded-for') || '';
+    //should log sesion ID when that gets implemented
+
+    //console.log(req.headers);
+    console.log(`${new Date().toISOString()} [${req.uuid}, ${host}, ${forwarded}] < ${req.method} ${req.originalUrl}`);
+    res.on('finish', ()=>{
+        console.log(`${new Date().toISOString()} [${req.uuid}, ${host}, ${forwarded}] > ${res.statusCode} ${res.statusMessage} ${res.get('Content-Length') || 0}b sent`);
+    });
     next();
 });
 
