895876425490f3c47e22a88a476e5bed6fd1346d
gary ramsey <gsramsey@gmail.com>
2018-11-19 22:41:35 -0500

    added in user model added in setting and verifying password to match PHP version

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const crypto = require('crypto');
+
+class User{
+    constructor(props){
+        let inputs = JSON.parse(props)[0];
+        this.name = inputs.name;
+        this.email = inputs.email;
+        this.password = inputs.password;
+        this.salt = inputs.salt;
+        this.requiresPasswordReset = inputs.requiresPasswordReset;
+    }
+    verifyPassword(input){
+        if(this.password === null || this.password === "" || this.salt === null || this.salt === ""
+        || input === null || input === ""){
+            return JSON({message: "Password and input can not be empty or null"});
+        }
+        let saltedInput = this.salt + input;
+        let hash = crypto.createHash('sha512').update(saltedInput).digest('hex');
+        //console.log(`Input: ${input}, hash: ${hash}, password: ${this.password}, match?: ${hash === this.password}`);
+        return hash === this.password;
+    }
+    setPassword(input){
+        let salt = crypto.randomBytes(32).toString('hex');
+        let saltedPass = salt + input;
+        this.password = crypto.createHash('sha512').update(saltedPass).digest('hex');
+        //console.log(`Input: ${input}, salt: ${salt}, password: ${this.password}`);
+    }
+}
+
+module.exports = User;
--- routes/users/single.js
+++ routes/users/single.js
@@ -1,3 +1,5 @@
+//const User = require('../../models/user.js');
+
 module.exports = (req, res)=>{
     //return a specific user
     res.locals.connection.query("SELECT * FROM users WHERE id=?", [req.params.id], (error, results, fields)=>{
@@ -5,6 +7,9 @@ module.exports = (req, res)=>{
         if(results.length === 0){
             res.status(404).send("Invalid user ID specified");
         }else{
+            //let user = new User(JSON.stringify(results));
+            //user.verifyPassword("Wr3tch3d");
+            //user.setPassword("Penis");
             res.status(200).send(JSON.stringify(results));            
         }
         res.locals.connection.end();
