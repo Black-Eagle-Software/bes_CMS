cc51a494c64547480274796f35696990a9e6ee1c
gary ramsey <gsramsey@gmail.com>
2018-11-22 22:20:39 -0500

    redid image tile to look closer to upload image tiles added hover effects to image tiles

--- client/src/components/image-tile.component.js
+++ client/src/components/image-tile.component.js
@@ -6,18 +6,35 @@ export default class ImageTile extends React.PureComponent{
     }
     
     render(){
-        const contStyle = {
+        /*const contStyle = {
             display: "inline-block",
             marginRight: "1em",
             marginBottom: "1em"
+        };*/
+        const contStyle = {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: "1em",
+            marginBottom: "1em",
+            width: "12.5em",
+            height: "12.5em",
+            overflow: "hidden"
         };
+
+        const imgStyle = {
+            flex: "1 1 auto",
+            maxWidth: "12.5em",
+            maxHeight: "12.5em",
+            objectFit: "contain"    //necessary?
+        }
 
         const imgSrc = this.props.media.thumb;
         const filename = this.props.media.file.originalFilename;
 
         return(
-            <div style={contStyle} onClick={()=>this.handleImageClick(this.props.media)}>
-                <img src={imgSrc} alt={filename} />
+            <div style={contStyle} className={"tile-bg"} onClick={()=>this.handleImageClick(this.props.media)}>
+                <img style={imgStyle} src={imgSrc} alt={filename} />
             </div>
         );
     }
