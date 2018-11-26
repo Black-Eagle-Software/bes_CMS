77e74ed101b01699773a7e610e1feef8c6d1eb5c
gary ramsey <gsramsey@gmail.com>
2018-11-25 20:34:23 -0500

    adds email query route to users

--- routes/users/all.js
+++ routes/users/all.js
@@ -1,7 +1,15 @@
 module.exports = (req, res)=>{
-    res.locals.connection.query("SELECT * FROM users", (error, results, fields)=>{
-        if(error) throw error;
-        res.status(200).send(JSON.stringify(results));                    
-        //res.locals.connection.end();
-    });
+    let email = req.query.email;
+    if(email){
+        res.locals.connection.query("SELECT * FROM users WHERE email=?", email, (error, results, fields)=>{
+            if(error) return done(error);
+            res.status(200).send(JSON.stringify(results));
+        });
+    }else{
+        res.locals.connection.query("SELECT * FROM users", (error, results, fields)=>{
+            if(error) throw error;
+            res.status(200).send(JSON.stringify(results));                    
+            //res.locals.connection.end();
+        });
+    }
 };
