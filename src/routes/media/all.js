70667b4629361e60ec8a2f23da52e2ae869f8ab3
gary ramsey <gsramsey@gmail.com>
2018-12-26 17:04:27 -0500

    fixes routes to return public results only (as originally intended)

--- src/routes/media/all.js
+++ src/routes/media/all.js
@@ -1,34 +1,19 @@
 module.exports = (req, res)=>{
     let limit = req.query.limit * 1;
-    if(req.isAuthenticated()){
-        if(limit){
-            res.locals.connection.query("SELECT * FROM media ORDER BY id DESC LIMIT ?", [limit], (error, results, fields)=>{
-                if(error) throw error;
-                res.status(200).send(JSON.stringify(results));
-                //res.locals.connection.end();
-            });
-        }else{
-            res.locals.connection.query("SELECT * FROM media", (error, results, fields)=>{
-                if(error) throw error;
-                res.status(200).send(JSON.stringify(results));            
-                //res.locals.connection.end();
-            });
-        }
+    //this should only return media that are public-viewable
+    //might need limit implementation too
+    //public media are tagged with only public tags
+    //+++This should pull out all media regardless of tag access level for admins
+    let queryStg = "SELECT * FROM media m INNER JOIN tagsToMediaMap tmm ON m.id = tmm.media INNER JOIN tagsToAccessLevelMap tam ON tmm.tag = tam.tagId WHERE tam.accessLevel = 'Public' ORDER BY id DESC";
+    if(limit){
+        res.locals.connection.query(queryStg + " LIMIT ?", [limit], (error, results, fields)=>{
+            if(error) throw error;
+            res.status(200).send(JSON.stringify(results));
+        });
     }else{
-        //this should only return media that are public-viewable
-        //might need limit implementation too
-        //public media are tagged with only public tags
-        let queryStg = "SELECT * FROM media m INNER JOIN tagsToMediaMap tmm ON m.id = tmm.media INNER JOIN tagsToAccessLevelMap tam ON tmm.tag = tam.tagId WHERE tam.accessLevel = 'Public' ORDER BY id ASC";
-        if(limit){
-            res.locals.connection.query(queryStg + " LIMIT ?", [limit], (error, results, fields)=>{
-                if(error) throw error;
-                res.status(200).send(JSON.stringify(results));
-            });
-        }else{
-            res.locals.connection.query(queryStg, (error, results, fields)=>{
-                if(error) throw error;
-                res.status(200).send(JSON.stringify(results));
-            });
-        }
+        res.locals.connection.query(queryStg, (error, results, fields)=>{
+            if(error) throw error;
+            res.status(200).send(JSON.stringify(results));
+        });
     }
 };
--- src/routes/tags/all.js
+++ src/routes/tags/all.js
@@ -1,33 +1,17 @@
 module.exports = (req, res)=>{    
     let limit = req.query.limit * 1;
-    if(req.isAuthenticated()){        
-        if(limit){
-            res.locals.connection.query("SELECT * FROM tags ORDER BY description ASC LIMIT ?", [limit], (error, results, fields)=>{
-                if(error) throw error;
-                res.status(200).send(JSON.stringify(results));
-                //res.locals.connection.end();
-            });
-        }else{
-            res.locals.connection.query("SELECT * FROM tags", (error, results, fields)=>{
-                if(error) throw error;
-                res.status(200).send(JSON.stringify(results));            
-                //res.locals.connection.end();
-            });
-        }
+    //this should only return tags that are public-viewable
+    //might need limit implementation too
+    let queryStg = "SELECT * FROM tags t INNER JOIN tagsToAccessLevelMap tam ON t.id = tam.tagId WHERE tam.accessLevel = 'Public' ORDER BY description ASC";
+    if(limit){
+        res.locals.connection.query(queryStg + " LIMIT ?", [limit], (error, results, fields)=>{
+            if(error) throw error;
+            res.status(200).send(JSON.stringify(results));
+        });
     }else{
-        //this should only return tags that are public-viewable
-        //might need limit implementation too
-        let queryStg = "SELECT * FROM tags t INNER JOIN tagsToAccessLevelMap tam ON t.id = tam.tagId WHERE tam.accessLevel = 'Public' ORDER BY description ASC";
-        if(limit){
-            res.locals.connection.query(queryStg + " LIMIT ?", [limit], (error, results, fields)=>{
-                if(error) throw error;
-                res.status(200).send(JSON.stringify(results));
-            });
-        }else{
-            res.locals.connection.query(queryStg, (error, results, fields)=>{
-                if(error) throw error;
-                res.status(200).send(JSON.stringify(results));
-            });
-        }
+        res.locals.connection.query(queryStg, (error, results, fields)=>{
+            if(error) throw error;
+            res.status(200).send(JSON.stringify(results));
+        });
     }
 };
