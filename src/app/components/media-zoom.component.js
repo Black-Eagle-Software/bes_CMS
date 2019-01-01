871e62fa387f496b465eb26ec19c370e392bf3ca
gary ramsey <gsramsey@gmail.com>
2018-12-31 21:21:17 -0500

    add downloading of media item and link to media details page

--- src/app/components/media-zoom.component.js
+++ src/app/components/media-zoom.component.js
@@ -55,6 +55,18 @@ export default class MediaZoom extends React.Component{
             color: "#1f1f1f",
             borderRadius: "0.5em"
         };
+        const dlSpanStyle = {
+            marginLeft: "0.5em"
+        };
+        const svgStyle = {
+            position: "relative",
+            top: "6px",
+            width: "24px",
+            height: "24px"
+        };
+        const filenameDivStyle = {
+            marginBottom: "0.5em"
+        };
 
         console.log(this.props.image_source);
         //this gets in a type as props.image_source.file.type
@@ -76,7 +88,24 @@ export default class MediaZoom extends React.Component{
                         <source src={this.props.image_source.src_file}/>
                     </video>
                 }
-                <span style={spanStyle}>{filename}</span>
+                <div style={filenameDivStyle}>
+                    <span style={spanStyle}>{filename}</span>
+                    <span style={dlSpanStyle}>
+                        <a className={"downloadLink"} href={this.props.image_source.src_file} download={filename} title={"Download media"}>
+                            <svg style={svgStyle}>
+                                <path d={"M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z"} />
+                            </svg>
+                        </a>
+                    </span>
+                    <span>
+                        <a className={"downloadLink"} href={`/media_details/${this.props.image_source.file.id}`} title={"Media details"}>
+                            <svg style={svgStyle}>
+                                <path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
+                            </svg>
+                        </a>
+                    </span>
+                    {/*TODO: this will want a link to the /media/id details page for this media item*/}
+                </div>
                 <span> 
                     {this.props.media_tags.map(tag=>{
                         return <span key={uuid()} style={spanTagStyle}>{tag.description}</span>
