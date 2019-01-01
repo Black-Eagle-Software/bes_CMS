dc76c37f4424dfd5f622972cddd012847336d0af
gary ramsey <gsramsey@gmail.com>
2018-12-31 22:47:49 -0500

    added checking of public tags and friends list for single media item query

--- src/routes/media/single.js
+++ src/routes/media/single.js
@@ -8,26 +8,55 @@ module.exports = (req, res)=>{
     let user = JSON.parse(req.user);
     console.log(user);
     console.log(`User ${user.id} is trying to view media item ${req.params.id}`);    
-    let qryString = "SELECT * FROM media WHERE id=?";
-    res.locals.connection.query(qryString, [req.params.id], (error, results, fields)=>{
+    //let qryString = "SELECT * FROM media WHERE id=?";
+    /*
+        SQL query in a better spacing/format
+        SELECT * FROM media m	
+        INNER JOIN tagsToMediaMap tmm 
+        ON tmm.media = m.id 
+        INNER JOIN tagsToAccessLevelMap tam 
+        ON tam.tagId = tmm.tag 
+        WHERE m.id = ? 
+        AND (m.owner = ? 
+            OR tam.accessLevel = 'Public' 
+            OR m.owner = (SELECT uuf.userId FROM usersToUsersFriendMap uuf WHERE uuf.friendId = ?) 
+            OR m.owner = (SELECT uuf.friendId FROM usersToUsersFriendMap uuf WHERE uuf.userId = ?))
+    */
+    let qryString  = "SELECT * FROM media m	INNER JOIN tagsToMediaMap tmm ON tmm.media = m.id INNER JOIN tagsToAccessLevelMap tam ON tam.tagId = tmm.tag WHERE m.id = ? AND (m.owner = ? OR tam.accessLevel = 'Public' OR m.owner = (SELECT uuf.userId FROM usersToUsersFriendMap uuf WHERE uuf.friendId = ?) OR m.owner = (SELECT uuf.friendId FROM usersToUsersFriendMap uuf WHERE uuf.userId = ?))"
+    res.locals.connection.query(qryString, [req.params.id, user.id, user.id, user.id], (error, results, fields)=>{
         if(error){
             res.status(404).send({'message': error.message});
             return;
         }
+        //console.log(results);
+        //console.log(results.length);
+        //console.log(results[0].count > 0);
+        //pull any tags this media has applied to it
         //confirm access to this media
         //0. check if  user is admin
         if(user.role === 'Administrator'){
             res.status(200).send(JSON.stringify(results[0]));
             return;
         }
-        //1. check if this media is tagged with a public tag
-        //2. check if requester is owner of media
-        //3. check if requester is friends of owner
+        /*//1. check if requester is owner of media
+        if(user.id === results[0].owner){
+            res.status(200).send(JSON.stringify(results[0]));
+            return;
+        }
+        //2a. check if this media is tagged with a public tag, or...                       
+        //2b. ...check if requester is friends of owner*/
         
-        if(user.id !== results[0].owner){   
+        /*if(user.id !== results[0].owner){   
+            res.status(403).send({'message': `User is not authorized to view media item ${req.params.id}`});
+            return;
+        }*/
+        //if we have a result, then we should be able to show it
+        if(results.length > 0){
+            res.status(200).send(JSON.stringify(results[0]));
+            return;
+        }else{
             res.status(403).send({'message': `User is not authorized to view media item ${req.params.id}`});
             return;
         }
-        res.status(200).send(JSON.stringify(results[0]));
     });    
 };
