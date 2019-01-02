13fd5003526c3410ebd42cb1337a5234809caa93
gary ramsey <gsramsey@gmail.com>
2019-01-01 20:54:31 -0500

    restructured dbase query to be more legible relocated admin check to ensure that it would actually get the proper results (admin has no friends, basically)

--- src/routes/media/single.js
+++ src/routes/media/single.js
@@ -7,22 +7,31 @@ module.exports = (req, res)=>{
     }*/
     let user = JSON.parse(req.user);
     console.log(user);
-    console.log(`User ${user.id} is trying to view media item ${req.params.id}`);    
+    console.log(`User ${user.id} is trying to view media item ${req.params.id}`);
+    if(user.role === 'Administrator'){
+        let qryString = "SELECT * FROM media m WHERE m.id = ?";
+        res.locals.connection.query(qryString, [req.params.id], (error, results, fields)=>{
+            if(error){
+                res.status(404).send({'message': error.message});
+                return;
+            }
+            res.status(200).send(JSON.stringify(results[0]));
+            return;
+        });
+    }    
     //let qryString = "SELECT * FROM media WHERE id=?";
-    /*
-        SQL query in a better spacing/format
-        SELECT * FROM media m	
-        INNER JOIN tagsToMediaMap tmm 
-        ON tmm.media = m.id 
-        INNER JOIN tagsToAccessLevelMap tam 
-        ON tam.tagId = tmm.tag 
-        WHERE m.id = ? 
-        AND (m.owner = ? 
-            OR tam.accessLevel = 'Public' 
-            OR m.owner = (SELECT uuf.userId FROM usersToUsersFriendMap uuf WHERE uuf.friendId = ?) 
-            OR m.owner = (SELECT uuf.friendId FROM usersToUsersFriendMap uuf WHERE uuf.userId = ?))
-    */
-    let qryString  = "SELECT * FROM media m	INNER JOIN tagsToMediaMap tmm ON tmm.media = m.id INNER JOIN tagsToAccessLevelMap tam ON tam.tagId = tmm.tag WHERE m.id = ? AND (m.owner = ? OR tam.accessLevel = 'Public' OR m.owner = (SELECT uuf.userId FROM usersToUsersFriendMap uuf WHERE uuf.friendId = ?) OR m.owner = (SELECT uuf.friendId FROM usersToUsersFriendMap uuf WHERE uuf.userId = ?))"
+    let qryString  = `SELECT * FROM media m	
+                        INNER JOIN tagsToMediaMap tmm 
+                        ON tmm.media = m.id 
+                        INNER JOIN tagsToAccessLevelMap tam 
+                        ON tam.tagId = tmm.tag 
+                        WHERE m.id = ? 
+                        AND (m.owner = ? 
+                            OR tam.accessLevel = 'Public' 
+                            OR m.owner = (SELECT uuf.userId FROM usersToUsersFriendMap uuf 
+                                            WHERE uuf.friendId = ?) 
+                            OR m.owner = (SELECT uuf.friendId FROM usersToUsersFriendMap uuf 
+                                            WHERE uuf.userId = ?))`    
     res.locals.connection.query(qryString, [req.params.id, user.id, user.id, user.id], (error, results, fields)=>{
         if(error){
             res.status(404).send({'message': error.message});
@@ -33,11 +42,6 @@ module.exports = (req, res)=>{
         //console.log(results[0].count > 0);
         //pull any tags this media has applied to it
         //confirm access to this media
-        //0. check if  user is admin
-        if(user.role === 'Administrator'){
-            res.status(200).send(JSON.stringify(results[0]));
-            return;
-        }
         /*//1. check if requester is owner of media
         if(user.id === results[0].owner){
             res.status(200).send(JSON.stringify(results[0]));
