38086f28c0dd98be610a793902f69f52d6e42626
gary ramsey <gsramsey@gmail.com>
2019-01-16 12:48:08 -0500

    changed get for user tags to include tag access level

--- src/routes/users/tags/all.js
+++ src/routes/users/tags/all.js
@@ -1,5 +1,5 @@
 module.exports = (req, res)=>{
-    res.locals.connection.query("SELECT * FROM tags WHERE owner=?", [req.params.id], (error, results, fields)=>{
+    res.locals.connection.query("SELECT * FROM tags t INNER JOIN tagsToAccessLevelMap tal ON tal.tagId = t.Id WHERE owner=?", [req.params.id], (error, results, fields)=>{
         if(error) throw error;
         if(results.length === 0){
             res.status(404).send("Invalid user ID specified or no tags exist");
