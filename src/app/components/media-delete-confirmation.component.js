7fc6d09ced1f726b57772f7fd9513a48bfd745a0
gary ramsey <gsramsey@gmail.com>
2019-01-16 15:33:44 -0500

    fix image overflowing

--- src/app/components/media-delete-confirmation.component.js
+++ src/app/components/media-delete-confirmation.component.js
@@ -17,7 +17,7 @@ export default class MediaDeleteConfirmation extends React.PureComponent{
             objectFit: "contain"
         };
         const imgStyle={
-            height: "100%",
+            maxHeight: "100%",
             maxWidth: "100%",
             boxShadow: "0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 12px 40px 0 rgba(0, 0, 0, 0.2)"
         };
