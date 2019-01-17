24b588ce99095396e0ac7e89c70de1c1669efba8
gary ramsey <gsramsey@gmail.com>
2019-01-17 15:10:39 -0500

    fixed user media get route to obey results limits

--- src/routes/users/media/all.js
+++ src/routes/users/media/all.js
@@ -1,11 +1,23 @@
 module.exports = (req, res)=>{
-    res.locals.connection.query("SELECT * FROM media WHERE owner=? ORDER BY id DESC", [req.params.id], (error, results, fields)=>{
-        if(error) throw error;
-        if(results.length === 0){
-            res.status(404).send("Invalid user ID specified or no media exists");
-        }else{
-            res.status(200).send(JSON.stringify(results));            
-        }
-        //res.locals.connection.end();
-    });
+    let limit = req.query.limit * 1;
+    let queryString = "SELECT * FROM media WHERE owner=? ORDER BY id DESC";
+    if(limit){
+        res.locals.connection.query(queryString + " LIMIT ?", [req.params.id, limit], (error, results, fields)=>{
+            if(error) throw error;
+            if(results.length === 0){
+                res.status(404).send("Invalid user ID specified or no media exists");
+            }else{
+                res.status(200).send(JSON.stringify(results));            
+            }
+        });
+    } else{
+        res.locals.connection.query(queryString, [req.params.id], (error, results, fields)=>{
+            if(error) throw error;
+            if(results.length === 0){
+                res.status(404).send("Invalid user ID specified or no media exists");
+            }else{
+                res.status(200).send(JSON.stringify(results));            
+            }
+        });
+    }    
 };
