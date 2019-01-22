19f51286e71115da5b7ab7fafee457f6b3eef54a
gary ramsey <gsramsey@gmail.com>
2019-01-22 18:19:26 -0500

    try and fix image sizing in dialog. still not 100% happy with this.

--- src/app/components/media-delete-confirmation.component.js
+++ src/app/components/media-delete-confirmation.component.js
@@ -14,12 +14,14 @@ export default class MediaDeleteConfirmation extends React.PureComponent{
             justifyContent: "center",
             flex: "1 1 auto",
             maxHeight: "50%",
-            objectFit: "contain"
+            /*objectFit: "contain"*/
         };
         const imgStyle={
             maxHeight: "100%",
             maxWidth: "100%",
-            boxShadow: "0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 12px 40px 0 rgba(0, 0, 0, 0.2)"
+            boxShadow: "0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 12px 40px 0 rgba(0, 0, 0, 0.2)",
+            objectFit: "contain",
+            flex: "1 1 auto"
         };
         const commandsStyle={
             display: "flex",
