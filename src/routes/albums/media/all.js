ed5899f1ef2da51157ec366b089e8b006a877ba8
gary ramsey <gsramsey@gmail.com>
2019-01-30 22:22:14 -0500

    allow for limiting results

--- src/routes/albums/media/all.js
+++ src/routes/albums/media/all.js
@@ -1,8 +1,17 @@
 module.exports = (req, res)=>{
-    let query = "SELECT * FROM media m INNER JOIN albumsToMediaMap amm ON m.id = amm.media WHERE amm.album = ?";
-    res.locals.connection.query(query, [req.params.id], (error, results, fields)=>{
-        if(error) throw error;
-        res.status(200).send(JSON.stringify(results));            
-        //res.locals.connection.end();
-    });
+    let limit = req.query.limit * 1;    
+    let query = "SELECT * FROM media m INNER JOIN albumsToMediaMap amm ON m.id = amm.media WHERE amm.album = ? ORDER BY m.id ASC";
+    if(limit){
+        res.locals.connection.query(query + " LIMIT ?", [req.params.id, limit], (error, results, fields)=>{
+            if(error) throw error;
+            res.status(200).send(JSON.stringify(results));
+            //res.locals.connection.end();
+        });
+    }else{
+        res.locals.connection.query(query, [req.params.id], (error, results, fields)=>{
+            if(error) throw error;
+            res.status(200).send(JSON.stringify(results));            
+            //res.locals.connection.end();
+        });
+    }
 };
