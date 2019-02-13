60dc3dddd870224f1b94ec0b592c025e866ea5bd
gary ramsey <gsramsey@gmail.com>
2019-02-12 20:10:08 -0500

    add onclick handler to media tile change media tile to extend purecomponent

--- src/app/components/media/media-tile.component.js
+++ src/app/components/media/media-tile.component.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import MediaTileImageContent from './media-tile-image-content.component';
 
-export default class MediaTile extends React.Component{
+export default class MediaTile extends React.PureComponent{
     render(){
         return(
-            <div className="media_tile-container">
+            <div className="media_tile-container" onClick={()=>this.props.onMediaClick()}>
                 <MediaTileImageContent  src={this.props.src}
                                         alt={this.props.title}
                                         imgClass={this.props.imgClass}/>
