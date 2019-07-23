9cd32c6c44a2711a7dba62bcaff71512d38022c4
gary ramsey <gsramsey@gmail.com>
2019-07-23 16:12:57 -0400

    change page url to not be hard-coded

--- src/helpers/windowNavigation.js
+++ src/helpers/windowNavigation.js
@@ -1,8 +1,5 @@
 export default class WindowNavigation{
     static goToLocation(location){
-        //this probably needs some hefty error handling
-        //this is also hard-coded for now and may need to be changed
-        //...but at least it's only in a single spot!
-        window.location.href = `http://10.0.5.15:8080${location}`;
+        window.location.href = `http://${window.location.hostname}:${window.location.port}${location}`;
     }
 }
