a541f90e35433d667cf5def50ec6cb0912be4e7b
gary ramsey <gsramsey@gmail.com>
2019-10-06 14:51:10 -0400

    fix album results not being sorted by album index

--- src/routes/albums/media/all.js
+++ src/routes/albums/media/all.js
@@ -1,6 +1,6 @@
 module.exports = (req, res)=>{
     let limit = req.query.limit * 1;    
-    let query = "SELECT * FROM media m INNER JOIN albumsToMediaMap amm ON m.id = amm.media WHERE amm.album = ? ORDER BY m.id ASC";
+    let query = "SELECT * FROM media m INNER JOIN albumsToMediaMap amm ON m.id = amm.media WHERE amm.album = ? ORDER BY amm.albumIndex ASC";
     if(limit){
         res.locals.connection.query(query + " LIMIT ?", [req.params.id, limit], (error, results, fields)=>{
             if(error) throw error;
